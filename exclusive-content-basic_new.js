37f5394788bcac7ebd23a5e0588dbefbdf925668
Glitch (wm2-exclusive-content-basic) <none>
2023-01-10 20:47:59 +0000

    🧰🕋 Updated with Glitch

---
+++ exclusive-content-basic_new.js
@@ -0,0 +1,11 @@
+if (window.MonetizationEvent) {
+ console.log("document monetization defined");
+
+//  const link = document.querySelector('link[rel="monetization"]');
+
+//  link.addEventListener("monetization", (ev) => {
+//    console.log("monetization started");
+
+    document.getElementById("exclusive").classList.remove("hidden");
+//  });
+}
